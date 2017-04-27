b49aea71fb9beeee6d3f9b1278206f89062b6a34
rjarratt <rjarratt@RJARRATT001>
2017-04-27 08:32:13 +0100

    Start of controller feature tests.

---
+++ src/Team-Services-Bot.AcceptanceTests/ControllerSteps.cs
@@ -0,0 +1,38 @@
+﻿// ———————————————————————————————
+// <copyright file="ControllerSteps.cs">
+// Licensed under the MIT License. See License.txt in the project root for license information.
+// </copyright>
+// <summary>
+// Contains the specflow steps to perform an echo.
+// </summary>
+// ———————————————————————————————
+
+namespace Vsar.TSBot.AcceptanceTests
+{
+    using System;
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class ControllerSteps
+    {
+        private MessagesController controller;
+
+        [Given(@"I have a controller")]
+        public void GivenIHaveAController()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I post a message activity to the controller")]
+        public void WhenIPostAMessageActivityToTheController()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the root dialog is invoked")]
+        public void ThenTheRootDialogIsInvoked()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ src/Team-Services-Bot.AcceptanceTests/Features/Controller.feature
@@ -0,0 +1,10 @@
+﻿Feature: Controller
+	As a chat user
+	I want to be able to invoke the Team Services Bot
+	So that I can get it to help me with Team Services tasks
+
+@Acceptance
+Scenario: Invoke the root dialog
+	Given I have a controller
+	When I post a message activity to the controller
+	Then the root dialog is invoked
