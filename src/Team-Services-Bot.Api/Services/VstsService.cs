00cdf8985a4acbeecf61f5cda5d8b4c771152318
Vladimir Gusarov <vgusarov@hotmail.com>
2017-06-12 16:49:58 +0300

    Added reading of build definitions.

--- src/Team-Services-Bot.Api/Services/VstsService.cs
+++ src/Team-Services-Bot.Api/Services/VstsService.cs
@@ -12,7 +12,9 @@ namespace Vsar.TSBot
     using System;
     using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
     using System.Threading.Tasks;
+    using Microsoft.TeamFoundation.Build.WebApi;
     using Microsoft.TeamFoundation.Core.WebApi;
     using Microsoft.VisualStudio.Services.Account;
     using Microsoft.VisualStudio.Services.Account.Client;
@@ -22,6 +24,7 @@ namespace Vsar.TSBot
     using Microsoft.VisualStudio.Services.ReleaseManagement.WebApi;
     using Microsoft.VisualStudio.Services.ReleaseManagement.WebApi.Clients;
     using Microsoft.VisualStudio.Services.WebApi;
+    using Vsar.TSBot.Resources;
 
     /// <summary>
     /// Contains method(s) for accessing VSTS.
@@ -138,6 +141,32 @@ namespace Vsar.TSBot
             }
         }
 
+        /// <inheritdoc/>
+        public async Task<IEnumerable<BuildDefinitionReference>> GetBuildDefinitionsAsync(string project, string account, OAuthToken token)
+        {
+            if (string.IsNullOrWhiteSpace(project))
+            {
+                throw new ArgumentNullException(nameof(project));
+            }
+
+            if (string.IsNullOrWhiteSpace(account))
+            {
+                throw new ArgumentNullException(nameof(account));
+            }
+
+            if (token == null)
+            {
+                throw new ArgumentNullException(nameof(token));
+            }
+
+            var teamProject = await this.GetProjectAsync(project, account, token);
+
+            using (var client = await GetConnectedClientAsync<BuildHttpClient>(new Uri(string.Format(VstsUrl, account)), token))
+            {
+                return await client.GetDefinitionsAsync(teamProject.Id);
+            }
+        }
+
         /// <summary>
         /// Gets a connected client.
         /// </summary>
@@ -163,6 +192,46 @@ namespace Vsar.TSBot
             }
 
             return client;
+        }
+
+        /// <summary>
+        /// Gets team project from VSTS account by name
+        /// </summary>
+        /// <param name="projectName">The team project name.</param>
+        /// <param name="account">The VSTS Account name.</param>
+        /// <param name="token">The <see cref="OAuthToken"/> for authentication.</param>
+        /// <returns><see cref="TeamProjectReference"/></returns>
+        private async Task<TeamProjectReference> GetProjectAsync(string projectName, string account, OAuthToken token)
+        {
+            var projects = await this.GetProjects(account, token);
+            var project = projects.FirstOrDefault(p => string.Equals(p.Name, projectName, StringComparison.OrdinalIgnoreCase));
+
+            if (project == default(TeamProjectReference))
+            {
+                throw new ArgumentOutOfRangeException(nameof(projectName), projectName, Exceptions.TeamProjectNotFound);
+            }
+
+            return project;
+        }
+
+        /// <summary>
+        /// Gets VSTS account by name
+        /// </summary>
+        /// <param name="accountName">The VSTS Account name.</param>
+        /// <param name="token">The <see cref="OAuthToken"/> for authentication.</param>
+        /// <param name="memberId">VSTS member ID.</param>
+        /// <returns><see cref="TeamProjectReference"/></returns>
+        private async Task<Account> GetAccountAsync(string accountName, OAuthToken token, Guid memberId)
+        {
+            var accounts = await this.GetAccounts(token, memberId);
+            var account = accounts.FirstOrDefault(a => string.Equals(a.AccountName, accountName, StringComparison.OrdinalIgnoreCase));
+
+            if (account == default(Account))
+            {
+                throw new ArgumentOutOfRangeException(nameof(accountName), accountName, Exceptions.AccountNotFound);
+            }
+
+            return account;
         }
     }
 }
