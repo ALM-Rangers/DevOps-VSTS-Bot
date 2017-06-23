99d7c5e066dc1a6f0209023015956abe850a8cd1
Jeffrey Opdam <j.opdam@leanphoenix.nl>
2017-06-23 20:59:47 +0200

    Added pages.

---
+++ src/Team-Services-Bot.Api/Controllers/DefaultController.cs
@@ -0,0 +1,37 @@
+﻿// ———————————————————————————————
+// <copyright file="DefaultController.cs">
+// Licensed under the MIT License. See License.txt in the project root for license information.
+// </copyright>
+// <summary>
+// Provides the controller logic for the default pages.
+// </summary>
+// ———————————————————————————————
+
+namespace Vsar.TSBot
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Provides the controller logic for the default pages.
+    /// </summary>
+    public class DefaultController : Controller
+    {
+        /// <summary>
+        /// Returns the default home page.
+        /// </summary>
+        /// <returns>A view.</returns>
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        /// <summary>
+        /// Returns the EULA page.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult EULA()
+        {
+            return this.View();
+        }
+    }
+}
