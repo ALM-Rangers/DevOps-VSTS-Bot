6f439b0e3fa6f74f6b2704cc888c3cfa346ebd18
Vladimir Gusarov <vgusarov@hotmail.com>
2017-06-24 22:37:03 +0300

    Minor formatting changes.

--- src/Team-Services-Bot.Api.UnitTests/Services/VstsServiceTest.cs
+++ src/Team-Services-Bot.Api.UnitTests/Services/VstsServiceTest.cs
@@ -9,7 +9,6 @@
 namespace Vsar.TSBot.UnitTests.Services
 {
     using System;
-    using System.Collections;
     using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
     using System.Linq;
@@ -133,6 +132,7 @@ namespace Vsar.TSBot.UnitTests.Services
         }
 
         [TestMethod]
+        [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "Test method shouldn't be a property. Test method name corresponds to method under test.")]
         public async Task GetBuildDefinitionsTest()
         {
             var service = new VstsService();
@@ -177,8 +177,10 @@ namespace Vsar.TSBot.UnitTests.Services
 
         private static void InitializeConnectionShim(VssHttpClientBase[] clients)
         {
-            ShimVssConnection.AllInstances.GetClientServiceImplAsyncTypeGuidFuncOfTypeGuidCancellationTokenTaskOfObjectCancellationToken =
-                (connection, type, arg3, arg4, cancellationToken) => Task.Run(() => (object)clients.FirstOrDefault(client => client.GetType() == type), cancellationToken);
+            ShimVssConnection.AllInstances
+                    .GetClientServiceImplAsyncTypeGuidFuncOfTypeGuidCancellationTokenTaskOfObjectCancellationToken =
+                (connection, type, arg3, arg4, cancellationToken) => Task.Run(
+                    () => (object)clients.FirstOrDefault(client => client.GetType() == type), cancellationToken);
         }
 
         private static ProfileHttpClient GetProfileHttpClient(Profile profile)
