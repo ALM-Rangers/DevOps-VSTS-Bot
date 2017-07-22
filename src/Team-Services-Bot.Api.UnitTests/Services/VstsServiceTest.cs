34e8ccbad16b17b95e79cb81413afe77fa55dc48
Jeffrey Opdam <j.opdam@leanphoenix.nl>
2017-07-22 22:34:41 +0200

    Renamed test name.

--- src/Team-Services-Bot.Api.UnitTests/Services/VstsServiceTest.cs
+++ src/Team-Services-Bot.Api.UnitTests/Services/VstsServiceTest.cs
@@ -489,7 +489,7 @@ namespace Vsar.TSBot.UnitTests.Services
 
         [TestMethod]
         [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "Test method shouldn't be a property. Test method name corresponds to method under test.")]
-        public async Task GetReleasesTest()
+        public async Task GetReleaseDefinitionsTest()
         {
             var accountName = "myaccount";
             var projectName = "myproject";
