c95d3a10689cad355454327639cdecd31a133934
Jeffrey Opdam <j.opdam@leanphoenix.nl>
2018-03-26 22:34:28 +0200

    Added terms and privacy pages

---
+++ src/VSTS-Bot.Api/Controllers/TermsController.cs
@@ -0,0 +1,28 @@
+﻿// ———————————————————————————————
+// <copyright file="TermsController.cs">
+// Licensed under the MIT License. See License.txt in the project root for license information.
+// </copyright>
+// <summary>
+// Provides the controller logic for the terms pages.
+// </summary>
+// ———————————————————————————————
+
+namespace Vsar.TSBot.Controllers
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Represents the terms controller
+    /// </summary>
+    public class TermsController : Controller
+    {
+        /// <summary>
+        /// Gets the terms page.
+        /// </summary>
+        /// <returns>A view.</returns>
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
